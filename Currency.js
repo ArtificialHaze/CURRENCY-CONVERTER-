6a862cff114e41e968a8b727517d05781373b02b
ArtificialHaze <83704626+ArtificialHaze@users.noreply.github.com>
2022-09-24 13:23:37 +0400

    Add files via upload

---
+++ Currency.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Currency = ({
+  currencyOptions,
+  selectedCurrency,
+  onChangeCurrency,
+  onChangeAmount,
+  amount,
+}) => {
+  return (
+    <div>
+      <input
+        type="number"
+        className="input"
+        value={amount}
+        onChange={onChangeAmount}
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Currency;
